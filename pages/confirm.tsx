ff822d9825532479dcf3194d00a7f8f4a8ceef22
francisamidu <francisamidu124@gmail.com>
2023-03-09 22:09:42 +0200

    Added subscribe page

--- pages/confirm.tsx
+++ pages/confirm.tsx
@@ -9,7 +9,7 @@ const Confirm = () => {
       <form className="w-[600px] shadow-md rounded flex flex-col items-center p-8">
         <MailCheck color="#0b5563" size={45} />
         <h1 className="text-4xl text-midnight-500 font-bold text-center my-4">
-          Become part of the {shared.name}
+          Become part of {shared.name}
         </h1>
         <p className="text-slate-700 text-center my-2">
           We have sent a confirmation email to you inbox. Confirm your email to
