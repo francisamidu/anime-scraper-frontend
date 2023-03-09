2cb0df59589384eca69b52a582b141ec5e3d0e4f
francisamidu <francisamidu124@gmail.com>
2023-03-09 22:13:40 +0200

    UI updates

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React, { PropsWithChildren } from "react";
 import shared from "../shared.json";
+import Footer from "./Footer";
 const { name, description } = shared;
 
 const Layout = ({ children }: Partial<PropsWithChildren>) => {
@@ -11,9 +12,10 @@ const Layout = ({ children }: Partial<PropsWithChildren>) => {
         <meta name="title" content={`${name}-${description}`}></meta>
         <meta name="description" content={description}></meta>
       </Head>
-      <main className="flex flex-col justify-between min-h-screen">
+      <main className="flex flex-col justify-between min-h-[95vh]">
         {children}
       </main>
+      <Footer />
     </>
   );
 };
