3539e64861d78de9aa2db4add19c1e4d008deb6f
francisamidu <francisamidu124@gmail.com>
2023-03-09 23:08:07 +0200

    Added subscribe func

--- components/SubscriptionForm.tsx
+++ components/SubscriptionForm.tsx
@@ -3,6 +3,7 @@ import { MainBtn } from ".";
 import shared from "../shared.json";
 
 const SubscriptionForm = () => {
+  const subscribeToNewsletter = async () => {};
   return (
     <form className="my-4">
       <h1 className="my-2 text-midnight-500 text-5xl font-bold text-center">
@@ -17,7 +18,7 @@ const SubscriptionForm = () => {
           placeholder="Enter your email address"
           className="w-[90%] p-2 rounded-lg border-blue-gray-50 border-[1px] mb-2 outline-none px-3"
         />
-        <MainBtn text="Subscribe Now" />
+        <MainBtn text="Subscribe Now" onClick={subscribeToNewsletter} />
       </div>
       <span className="text-blue-gray-600 md:text-left text-center w-full">
         Get weekly updates in your inbox. Don{`'`}t worry we don{`'`}t spam!!
