af350af2924eccefd2267a559e1dd8c33d442e7a
francisamidu <francisamidu124@gmail.com>
2023-03-16 20:53:16 +0200

    update unsubscribe endpoint

---
+++ pages/api/unsubscribe.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fetcher from "../../utils/fetcher";
+import shared from "../../shared.json";
+import { ResponseData } from "./subscribe";
+
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    email: string;
+    conf_num: string;
+  };
+}
+
+export default async function handler(
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse<ResponseData | string>
+) {
+  const { email, conf_num } = req.body;
+  if (!email) {
+    res.status(429).json({
+      result: "Please provide your email",
+    });
+  }
+
+  if (!conf_num) {
+    res.status(429).json({
+      result: "Please provide your confirmation number",
+    });
+  }
+
+  const response = await fetcher<ResponseData>(
+    `${shared.api}/newsletter/unsubscribe`,
+    {
+      method: "POST",
+    }
+  );
+  res.status(200).json({
+    result: response.result,
+  });
+}
