89d73d0dc8da4f56817ece5a4002da184369cc55
francisamidu <francisamidu124@gmail.com>
2023-03-16 20:18:48 +0200

    subscribe endpoint

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,7 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
 export type ResponseData = {
   result: string;
 };
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    email: string;
+    firstName: string;
+  };
+}
 export default async function handler(
-  req: NextApiRequest,
+  req: ExtendedNextApiRequest,
   res: NextApiResponse<ResponseData | string>
-) {}
+) {
+  const { email, firstName } = req.body;
+}
