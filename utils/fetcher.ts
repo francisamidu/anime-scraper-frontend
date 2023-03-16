cf83f493e135867fcbab87c073371590e0ab7fa8
francisamidu <francisamidu124@gmail.com>
2023-03-16 20:21:12 +0200

    added fetcher

---
+++ utils/fetcher.ts
@@ -0,0 +1,9 @@
+function fetcher<TResponse>(
+  url: string,
+  config: RequestInit = {}
+): Promise<TResponse> {
+  return fetch(url, config)
+    .then((response) => response.json())
+    .then((data) => data as TResponse);
+}
+export default fetcher;
