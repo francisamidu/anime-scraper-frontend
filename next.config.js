5eeb243fe70a5cd21ebd0a43bb665ab7b7177309
francisamidu <francisamidu124@gmail.com>
2023-03-21 17:56:57 +0200

    prefixed env variables with NEXT_PUBLIC

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   reactStrictMode: true,
   publicRuntimeConfig: {
-    CONFIRMATION_URL: process.env.CONFIRMATION_URL,
-    SUBSCRIPTION_URL: process.env.SUBSCRIPTION_URL,
-    UNSUBSCRIPTION_URL: process.env.UNSUBSCRIPTION_URL
+    CONFIRMATION_URL: process.env.NEXT_PUBLIC_CONFIRMATION_URL,
+    SUBSCRIPTION_URL: process.env.NEXT_PUBLIC_SUBSCRIPTION_URL,
+    UNSUBSCRIPTION_URL: process.env.NEXT_PUBLIC_UNSUBSCRIPTION_URL
   }
 }
